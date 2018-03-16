ec62fdc3adae340410b76940916309934cfc13bb
lucatorsera <lucatorsera@gmail.com>
2018-03-16 16:32:21 +0100

    Update homeWaves.js

--- assets/sketches/homeWaves.js
+++ assets/sketches/homeWaves.js
@@ -25,7 +25,7 @@ function setup() {
 }
 
 function draw() {
-  cor = (random(255), random(255), random(255));
+  cor = color((random(255), random(255), random(255)));
   
   for (var y = 0; y < height; y++){
     var noiseVal = noise(posY*noiseScale, (posY + y)*noiseScale);
