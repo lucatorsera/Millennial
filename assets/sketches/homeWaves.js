3ce090d9a6a83ae0534a66afa9f6c044e5f548a5
lucatorsera <lucatorsera@gmail.com>
2018-03-24 18:45:21 +0100

    Create homeWaves.js

---
+++ assets/sketches/homeWaves.js
@@ -0,0 +1,54 @@
+//No MIC
+
+var noiseScale = 0.02;
+var posY = 0.0;
+var posX = [];
+var cor = [];
+var NUM_STRIPS = 6;
+var speed = 0.7;
+
+
+function setup() {
+  // find the size of the underlying div
+  var divWidth = $("#setup_p5_sketch").width();
+  var divHeight = $("#setup_p5_sketch").height();
+  var myCanvas = createCanvas(divWidth, divHeight);
+  myCanvas.parent('setup_p5_sketch');
+  //myCanvas.position(0, 0);
+  //myCanvas.style('z-index', '-1');
+  frameRate(60);
+  posX[0] = random(10);
+  for(var i = 1; i < NUM_STRIPS; i++){
+    posX[i] = posX[i - 1] + random(20);
+  }
+  for(var i = 0; i < NUM_STRIPS; i++){
+    cor[i] = color(random(255), random(255), random(255));
+  }
+
+
+  background(255);
+  
+}
+
+function draw() {
+  background(255);
+  
+  for(var i = posX.length - 1; i >= 0; i--){
+    for(var y = 0; y < height; y++){
+      var noiseVal = noise(posY*noiseScale, (posY + y)*noiseScale);
+      stroke(cor[i]);
+      line(width - (posX[i] + noiseVal*100 + 40), y, width, y);
+      line(0, y, posX[i] + noiseVal*100, y);
+    }
+  }
+
+  posY+=speed;
+  
+}
+
+// when the window is resized the canvas is resized accordingly
+function windowResized(){
+  var divWidth = $("#setup_p5_sketch").width();
+  var divHeight = $("#setup_p5_sketch").height();
+  resizeCanvas(divWidth, divHeight);
+}
